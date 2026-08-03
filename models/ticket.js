0a47862ceea24a367d0bc6e7f97e03928badee85
Hacker_luna <165921766+Lunaluki@users.noreply.github.com>
2026-08-03 15:35:05 +0200

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -6,7 +6,12 @@ const ticketSchema = new mongoose.Schema({
   subject: { type: String, required: true },
   message: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  status: { type: String, default: "open" }
+
+  status: { 
+    type: String, 
+    enum: ["open", "processing", "closed"], 
+    default: "open" 
+  }
 });
 
 export default mongoose.model("Ticket", ticketSchema);
