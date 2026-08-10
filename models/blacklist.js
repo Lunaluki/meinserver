5f0848a98757d4d64703b9ea3ef4b2f58e55707a
Hacker_luna <165921766+Lunaluki@users.noreply.github.com>
2026-08-10 14:50:13 +0200

    Update blacklist.js

--- models/blacklist.js
+++ models/blacklist.js
@@ -6,7 +6,8 @@ const blacklistSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Verknüpfung zum echten Account
   reason: { type: String, default: "Kein Grund angegeben" },
   count: { type: Number, default: 1 }, // Zähler für Mehrfach-Meldungen
-  reporters: { type: [String], default: [] }, // Schutz vor doppelten Meldungen (IPs, User-IDs, Namen)
+  reporters: { type: [String], default: [] }, // Schutz vor doppelten Meldungen
+  screenshots: { type: [String], default: [] }, // 📸 NEU: Speichert die Bild-Pfade/URLs
   createdAt: { type: Date, default: Date.now }
 });
 
