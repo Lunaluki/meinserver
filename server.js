0764550fdeabad30274362d8028e0cdce5b8490b
Hacker_luna <165921766+Lunaluki@users.noreply.github.com>
2026-09-08 12:14:44 +0200

    Aktualisieren von server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.set('trust proxy', true);
 
 // 🔗 MailWatcher URL über Environment Variable oder Fallback
-const MAILWATCHER = process.env.MAILWATCHER_URL || "https://ireland-versus-metric-biotechnology.trycloudflare.com";
+const MAILWATCHER = process.env.MAILWATCHER_URL || "https://newspapers-reservoir-grown-joseph.trycloudflare.com";
 
 // 📂 Sicherstellen, dass der Upload-Ordner existiert
 const uploadDir = "uploads";
