402a9fe3bb0a9cf3958d911ae0c3118e7e775644
Hacker_luna <165921766+Lunaluki@users.noreply.github.com>
2026-09-10 09:53:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const io = new Server(server, {
 // =========================================================
 // 🌐 ZENTRALE KONFIGURATION (Direkt im Code hinterlegt)
 // =========================================================
-const MAILWATCHER = process.env.MAILWATCHER_URL || "https://essays-radar-residence-attempted.trycloudflared.com";
+const MAILWATCHER = process.env.MAILWATCHER_URL || "https://transmit-shore-feedback-mean.trycloudflared.com";
 
 // CORS komplett öffnen
 app.use(cors({
